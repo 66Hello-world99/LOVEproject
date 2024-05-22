f6e40d3d3989b31c1574b4a5a0c02e074ef28a39
sonny <168699848+66Hello-world99@users.noreply.github.com>
2024-05-22 19:05:04 +0900

    소스.cpp

--- LOVEproject/소스.cpp
+++ LOVEproject/소스.cpp
@@ -1,4 +1,17 @@
 #include<iostream>
+#include<string>
 using namespace std;
 
-int mai
+int main() {
+	string findLove = "";
+	while (1) {
+		char newChar= rand() % 26 + 65;
+		findLove += newChar;
+		cout << newChar;
+		if (findLove.size()>=4&&findLove.substr(findLove.size() - 4)=="LOVE") {
+			break;
+		}
+	}
+
+	cout << "\n\nLOVE found in " << findLove.size() << " letters\n";
+}
